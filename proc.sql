24bba1927a78e2ada138c08f78aae7b2c5ed3897
Cansisti <44342372+Cansisti@users.noreply.github.com>
2019-01-13 19:27:59 +0100

    pop. zakładanie bazy, procedury dodawania

---
+++ proc.sql
@@ -0,0 +1,106 @@
+drop procedure if exists dodaj_wlasciciela;
+delimiter $$
+
+create procedure dodaj_wlasciciela(
+	in dlogin varchar(20),
+	in dhaslo varchar(50),
+	out ret varchar(50)
+)
+begin
+	declare exit handler for 1062 set ret = "login alerady in use";
+	insert into wlasciciele values( null, dlogin, sha2( dhaslo, 256 ) );
+	set ret = "success";
+end$$
+
+delimiter ;
+
+drop procedure if exists dodaj_obiekt;
+delimiter $$
+
+create procedure dodaj_obiekt(
+	in idw int(12),
+	adres varchar(50),
+	msie smallint(5) unsigned,
+	msto smallint(5) unsigned,
+	in dlogin varchar(20),
+	in dhaslo varchar(50),
+	out ret varchar(50)
+)
+__:begin
+	declare _login varchar(20);
+	declare _haslo varchar(70);
+	declare exit handler for 1452 set ret = "id_wlasciciela not found";
+	select login, haslo into _login, _haslo from wlasciciele where id_wlasciciela = idw;
+	if _login != dlogin or _haslo != sha2( dhaslo, 256 ) then
+		set ret = "authentication failed";
+		leave __;
+	end if;
+	insert into obiekty values( null, idw, adres, msie, msto );
+	set ret = "success";
+end$$
+
+delimiter ;
+
+drop procedure if exists dodaj_koncert;
+delimiter $$
+
+create procedure dodaj_koncert(
+	id_obiektu int(12),
+	id_zespolu int(12),
+	data_koncertu date,
+	data_sprzedarzy date,
+	in dlogin varchar(20),
+	in dhaslo varchar(50),
+	out ret varchar(50)
+)
+__:begin
+	declare _login varchar(20);
+	declare _haslo varchar(70);
+	declare exit handler for 1452 set ret = "id_obiektu or id_zespolu not found";
+	select login, haslo into _login, _haslo from wlasciciele join obiekty where obiekty.id_wlasciciela = wlasciciele.id_wlasciciela and obiekty.id_obiektu = id_obiektu;
+	if _login != dlogin or _haslo != sha2( dhaslo, 256 ) then
+		set ret = "authentication failed";
+		leave __;
+	end if;
+	insert into koncerty values( null, id_obiektu, id_zespolu, data_koncertu, data_sprzedarzy, 0, 0, 0 );
+	set ret = "success";
+end$$
+
+delimiter ;
+
+drop procedure if exists dodaj_bilety;
+delimiter $$
+
+create procedure dodaj_bilety(
+	num int(12),
+	idk int(12),
+	cena int(4),
+	rodzaj_miejsca enum ('siedzace', 'stojace'),	
+	in dlogin varchar(20),
+	in dhaslo varchar(50),
+	out ret varchar(50)
+)
+__:begin
+	declare _login varchar(20) default "";
+	declare _haslo varchar(70) default "";
+	declare cnt int default 0;
+	declare exit handler for 1452 set ret = "id_koncertu not found";
+	declare exit handler for 1048 set ret = "forbidden null value";
+	declare exit handler for 1265 set ret = "wrong rodzaj_miejsca value";
+	select login, haslo into _login, _haslo
+		from wlasciciele
+		join obiekty on obiekty.id_wlasciciela = wlasciciele.id_wlasciciela
+		join koncerty on koncerty.id_obiektu = obiekty.id_obiektu
+		where koncerty.id_koncertu = idk;
+	if _login != dlogin or _haslo != sha2( dhaslo, 256 ) then
+		set ret = "authentication failed";
+		leave __;
+	end if;
+	while cnt < num do
+		insert into bilety values( null, idk, cena, rodzaj_miejsca, false );
+		set cnt = cnt+1;
+	end while;
+	set ret = "success";
+end$$
+
+delimiter ;
---
+++ tabele.sql
@@ -0,0 +1,71 @@
+DROP DATABASE IF EXISTS livewroclaw2;
+CREATE DATABASE livewroclaw2;
+
+USE livewroclaw2;
+
+CREATE TABLE wlasciciele (
+  id_wlasciciela int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  login          varchar(20) NOT NULL UNIQUE,
+  haslo          varchar(70),
+  PRIMARY KEY (id_wlasciciela)
+);
+
+CREATE TABLE obiekty (
+  id_obiektu           int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  id_wlasciciela       int(12) NOT NULL,
+  adres                varchar(50),
+  il_miejsc_siedzacych smallint(5) unsigned,
+  il_miejsc_stojacych  smallint(5) unsigned,
+  PRIMARY KEY (id_obiektu),
+  FOREIGN KEY (id_wlasciciela) REFERENCES wlasciciele (id_wlasciciela)
+);
+
+CREATE TABLE koncerty (
+  id_koncertu            int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  id_obiektu             int(12) NOT NULL,
+  id_zespolu             int(12) NOT NULL,
+  data_koncertu          date,
+  data_sprzedarzy        date,
+  il_miejsc_siedzacych   smallint(5) unsigned,
+  il_miejsc_stojacych    smallint(5) unsigned,
+  il_pozostalych_biletow smallint(5) unsigned,
+  PRIMARY KEY (id_koncertu),
+  FOREIGN KEY (id_obiektu) REFERENCES obiekty (id_obiektu)
+);
+
+CREATE TABLE bilety (
+  id_biletu      int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  id_koncertu    int(12) NOT NULL,
+  cena           int(4)  NOT NULL,
+  rodzaj_miejsca enum ('siedzace', 'stojace'),
+  czy_sprzedany  boolean,
+  PRIMARY KEY (id_biletu)
+);
+
+CREATE TABLE zespoly (
+  id_zespolu int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  nazwa_zespolu varchar(40) NOT NULL UNIQUE,
+  kategoria varchar(100),
+  PRIMARY KEY (id_zespolu)
+);
+
+CREATE TABLE komentarze_koncerty (
+  id_kom_koncertu int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  id_koncertu int(12) NOT NULL,
+  ocena_koncertu smallint(3),
+  kom_koncertu_tekst varchar(200),
+  PRIMARY KEY (id_kom_koncertu),
+  FOREIGN KEY (id_koncertu) REFERENCES koncerty (id_koncertu)
+);
+
+CREATE TABLE komentarze_obiektu (
+  id_kom_obiektu int(12) NOT NULL UNIQUE AUTO_INCREMENT,
+  id_obiektu int(12) NOT NULL,
+  ocena_obiektu smallint(3),
+  kom_obiektu_tekst varchar(200),
+  PRIMARY KEY (id_kom_obiektu),
+  FOREIGN KEY (id_obiektu) REFERENCES obiekty (id_obiektu)
+);
+
+ALTER TABLE koncerty
+ADD FOREIGN KEY (id_zespolu) REFERENCES zespoly(id_zespolu);
