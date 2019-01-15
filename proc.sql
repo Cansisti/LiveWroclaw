e8d3ef0cf3bb954b738bc48c8df95b35bf2880e5
Cansisti <44342372+Cansisti@users.noreply.github.com>
2019-01-15 12:57:49 +0100

    dodawanie z nazwą obiektu, sprawdzanie maks. ilości miejsc

--- proc.sql
+++ proc.sql
@@ -18,7 +18,8 @@ drop procedure if exists dodaj_obiekt;
 delimiter $$
 
 create procedure dodaj_obiekt(
-	in idw int(12),
+	nazwa varchar(50),
+	idw int(12),
 	adres varchar(50),
 	msie smallint(5) unsigned,
 	msto smallint(5) unsigned,
@@ -35,7 +36,7 @@ __:begin
 		set ret = "authentication failed";
 		leave __;
 	end if;
-	insert into obiekty values( null, idw, adres, msie, msto );
+	insert into obiekty values( null, idw, adres, msie, msto, nazwa );
 	set ret = "success";
 end$$
 
@@ -75,7 +76,7 @@ create procedure dodaj_bilety(
 	num int(12),
 	idk int(12),
 	cena int(4),
-	rodzaj_miejsca enum ('siedzace', 'stojace'),	
+	rodzaj_miejsca enum ('siedzace', 'stojace'),
 	in dlogin varchar(20),
 	in dhaslo varchar(50),
 	out ret varchar(50)
@@ -83,10 +84,26 @@ create procedure dodaj_bilety(
 __:begin
 	declare _login varchar(20) default "";
 	declare _haslo varchar(70) default "";
-	declare cnt int default 0;
+	declare cnt int default 0; -- licznik do pętli
+	declare cr int default 0; -- aktualna ilość biletów
+	declare mx int default 0; -- maksymalna ilość biletów
 	declare exit handler for 1452 set ret = "id_koncertu not found";
 	declare exit handler for 1048 set ret = "forbidden null value";
 	declare exit handler for 1265 set ret = "wrong rodzaj_miejsca value";
+	-- pobieramy max
+	if rodzaj_miejsca = 'siedzace' then
+		select obiekty.il_miejsc_siedzacych into mx from obiekty join (select * from koncerty where id_koncertu = idk ) kon on obiekty.id_obiektu = kon.id_obiektu;
+	else
+		select obiekty.il_miejsc_stojacych into mx from obiekty join (select * from koncerty where id_koncertu = idk ) kon on obiekty.id_obiektu = kon.id_obiektu;
+	end if;
+	-- liczymy aktualną
+	select count( id_biletu ) into cr from bilety where bilety.id_koncertu = idk and bilety.rodzaj_miejsca = rodzaj_miejsca;
+	-- if cr+num > mx return error
+	if num+cr > mx then
+		set ret = "num too big";
+		leave __;
+	end if;
+	-- autoryzacja
 	select login, haslo into _login, _haslo
 		from wlasciciele
 		join obiekty on obiekty.id_wlasciciela = wlasciciele.id_wlasciciela
