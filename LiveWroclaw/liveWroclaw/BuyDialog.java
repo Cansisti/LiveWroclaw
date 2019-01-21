7133b6441262ea3d8595cbb5208b51357c9c9b2e
dagmaragluch <44326629+dagmaragluch@users.noreply.github.com>
2019-01-21 03:16:06 +0100

    ....

--- LiveWroclaw/liveWroclaw/BuyDialog.java
+++ LiveWroclaw/liveWroclaw/BuyDialog.java
@@ -12,78 +12,103 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.Types;
 
 public class BuyDialog extends WindowAdapter implements ActionListener, ItemListener {
 
-	Dialog dialog;
-	
-	int id_koncertu;
-	
-	Choice typ;
-	TextField ilosc;
-	Button accept;
-	Label status;
-	
-	BuyDialog( int idk ) {
-		id_koncertu = idk;
-		dialog = new Dialog( App.frame );
-		dialog.addWindowListener( this );
-		dialog.setBounds( 350, 150, 500, 200 );
-		dialog.setLayout( new GridLayout( 6, 1 ) );
-		
-		status = new Label();
-		typ = new Choice();
-		ilosc = new TextField();
-		accept = new Button( "Akceptuj" );
+    Dialog dialog;
 
-		accept.addActionListener( this );
-		typ.add( "Siedzące" );
-		typ.add( "Stojące" );
-		typ.addItemListener( this );
-		
-		dialog.add( new Label( "Ilość:" ) );
-		dialog.add( ilosc );
-		dialog.add( new Label( "Typ: " ) );
-		dialog.add( typ );
-		dialog.add( status );
-		dialog.add( accept );
-		
-		
-		dialog.setVisible( true );
-	}
+    int id_koncertu;
 
-	@Override
-	public void actionPerformed( ActionEvent e ) {
-		if( e.getSource() == accept ) {
-			try {
-				int ilosc_biletow = Integer.parseInt( ilosc.getText() );
-				int rodzaj_miejsca = typ.getSelectedIndex(); // 0 to siedzące, 1 to stojące
-				// id_koncertu jest dane
-				// TODO
-				// kupić bilety
-			}
-			catch( NumberFormatException ex ) {
-				status.setText( "Ilość musi być liczbą" );
-			}
-		}
-	}
+    Choice typ;
+    TextField ilosc;
+    Button accept;
+    Label status;
 
-	@Override
-	public void itemStateChanged(ItemEvent arg0) {
-		// TODO
-		int cena = 0;
-		if( arg0.getItem().equals( "siedzące" ) ) {
-			// TODO
-		}
-		if( arg0.getItem().equals( "stojące" ) ) {
-			// TODO
-		}
-		status.setText( "Cena: " + cena );
-	}
-	
-	
-	@Override
-	public void windowClosing( WindowEvent e ) {
-		dialog.dispose();
-	}
+    BuyDialog(int idk) {
+        id_koncertu = idk;
+        dialog = new Dialog(App.frame);
+        dialog.addWindowListener(this);
+        dialog.setBounds(350, 150, 500, 200);
+        dialog.setLayout(new GridLayout(6, 1));
+
+        status = new Label();
+        typ = new Choice();
+        ilosc = new TextField();
+        accept = new Button("Akceptuj");
+
+        accept.addActionListener(this);
+        typ.add("Siedzące");
+        typ.add("Stojące");
+        typ.addItemListener(this);
+
+        dialog.add(new Label("Ilość:"));
+        dialog.add(ilosc);
+        dialog.add(new Label("Typ: "));
+        dialog.add(typ);
+        dialog.add(status);
+        dialog.add(accept);
+
+
+        dialog.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == accept) {
+            try {
+                int ilosc_biletow = Integer.parseInt(ilosc.getText());
+                int rodzaj_miejsca = typ.getSelectedIndex(); // 0 to siedzące, 1 to stojące
+                // id_koncertu jest dane
+                // TODO
+                // kupić bilety
+
+                CallableStatement cstmt = App.conn.prepareCall("call kup_bilet2( ?, ?, ?)");
+                cstmt.setInt(1, id_koncertu);
+                cstmt.setInt(2, rodzaj_miejsca);
+                cstmt.setInt(3, ilosc_biletow);
+                cstmt.executeUpdate();
+
+
+//                CallableStatement cstmt = App.conn.prepareCall("call kup_bilet( ?, ?, ?)");
+//                int c = 0;
+//                cstmt.registerOutParameter(1, Types.INTEGER);
+//                cstmt.execute();
+//                c = cstmt.getInt(1);
+//                System.out.println(c);
+
+
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            } catch (NumberFormatException ex) {
+                status.setText("Ilość musi być liczbą");
+            }
+        }
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent arg0) {
+        // TODO
+
+        int cena = 0;
+        if (arg0.getItem().equals("Siedzące")) {
+            // TODO
+            typ.select(0);
+
+        }
+        if (arg0.getItem().equals("Stojące")) {
+            // TODO
+            typ.select(1);
+
+        }
+        status.setText("Cena: " + cena);
+    }
+
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        dialog.dispose();
+    }
 }
--- zakupBiletu1.sql
+++ zakupBiletu1.sql
@@ -1,32 +1,37 @@
+USE livewroclaw2;
+
 DROP PROCEDURE IF EXISTS kup_bilet;
 
 
 DELIMITER $$
-CREATE FUNCTION kup_bilet(rodzaj enum ('stojace', 'siedzace'), ilosc smallint(1))
+CREATE FUNCTION kup_bilet(id int(12), rodzaj enum ('stojace', 'siedzace'), ilosc smallint(1))
 
-    RETURNS int(4)
-NOT DETERMINISTIC
+  RETURNS int(4)
+  NOT DETERMINISTIC
 
-  BEGIN
-   DECLARE suma int(12);
-    SET @i = 0;
+BEGIN
+  DECLARE suma int(12);
+  SET @i = 0;
 
-   SET suma = (SELECT SUM(cena) FROM bilety WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0);
+  SET suma = (SELECT SUM(cena) FROM bilety WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0);
 
-    SET autocommit = 0;
-    START TRANSACTION;
+  SET autocommit = 0;
+  START TRANSACTION;
 
-    WHILE @i <= ilosc
-    DO
-      UPDATE bilety SET czy_sprzedany = 1 WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0;
+  WHILE @i <= ilosc
+  DO
+  UPDATE bilety
+    JOIN koncerty ON bilety.id_koncertu = koncerty.id_koncertu
+  SET czy_sprzedany = 1
+  WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0 AND bilety.id_koncertu = id ;
 
-    END WHILE;
+  END WHILE;
 
-    COMMIT;
+  COMMIT;
 
-    RETURN suma;
+  RETURN suma;
 
-  END $$
+END $$
 DELIMITER ;
 
 /****************/
@@ -34,20 +39,23 @@ DROP PROCEDURE IF EXISTS kup_bilet2;
 
 
 DELIMITER $$
-CREATE PROCEDURE kup_bilet2(rodzaj enum ('stojace', 'siedzace'), ilosc smallint(1))
+CREATE PROCEDURE kup_bilet2(id int(12), rodzaj enum ('stojace', 'siedzace'), ilosc smallint(1))
 
-  BEGIN
-    SET @i = 0;
+BEGIN
+  SET @i = 0;
 
-    SET autocommit = 0;
-    START TRANSACTION;
+  SET autocommit = 0;
+  START TRANSACTION;
 
-    WHILE @i <= ilosc
-    DO
-      UPDATE bilety SET czy_sprzedany = 1 WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0;
-    END WHILE;
+  WHILE @i <= ilosc
+  DO
+  UPDATE bilety
+    JOIN koncerty ON bilety.id_koncertu = koncerty.id_koncertu
+  SET czy_sprzedany = 1
+  WHERE rodzaj_miejsca = rodzaj AND czy_sprzedany = 0 AND bilety.id_koncertu = id ;
+  END WHILE;
 
-    COMMIT;
+  COMMIT;
 
-  END $$
+END $$
 DELIMITER ;
